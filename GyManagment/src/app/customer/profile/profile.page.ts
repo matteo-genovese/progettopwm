2e8add907645019ddca9200b9a473122c5545793
matteo-genovese <matteo.genovese2003@gmail.com>
2025-06-13 17:23:49 +0200

    fix: add missing AlertController and ToastController imports in profile page

--- GyManagment/src/app/customer/profile/profile.page.ts
+++ GyManagment/src/app/customer/profile/profile.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, 
-  IonItem, IonLabel, IonButton, IonIcon, IonList, IonSpinner, IonAvatar, IonButtons, IonBackButton
+  IonItem, IonLabel, IonButton, IonIcon, IonList, IonSpinner, IonAvatar, AlertController, ToastController, IonButtons
 } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { 
